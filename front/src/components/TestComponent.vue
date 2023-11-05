24cadf36e82b4eb101a43903f8c4b8620761dd0b
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2023-11-05 14:23:23 +0900

    画面側を簡易的に実装

---
+++ front/src/components/TestComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    これはコンポーネントから表示されています。
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['message']
+  }
+</script>
---
+++ front/src/views/searchEvent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>     
+    <h1>もくもく探すくん</h1>
+    <form @submit="searchEvents">
+      <label for="keywords">キーワード検索:</label>
+      <input type="text" id="keywords" v-model="keyword" placeholder="キーワードを入力">
+
+      <label for="ym">年月検索:</label>
+      <input type="text" id="ym" v-model="ym" placeholder="yyyymm">
+
+      <button type="submit">検索</button>
+    </form>
+  </div>
+</template> 
+
+<script>
+export default {
+  data() {
+    return {
+      keyword: "", // キーワード
+      ym: "",      // 年月
+    };
+  },
+  methods: {
+    searchEvents() {
+      // サーバーサイドへのリクエストを送信し、検索結果を取得するロジックをここに実装
+      // バックエンドにリクエストを送信する際に、キーワードと年月の値を使ってAPIリクエストを作成
+    }
+  },
+}
+</script>
